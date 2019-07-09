be1b19c2c6272efde504dfbb05e4bb21568285b8
rachata101 <50059516+rachata101@users.noreply.github.com>
2019-07-09 19:59:33 +0700

    Create EX4.py

---
+++ EX4.py
@@ -0,0 +1,11 @@
+Found_eng = 60
+General_bus = 60
+Intro_com = 60
+Com_prog = 60
+
+print("---Your Score---")
+print("Foundation English :",Found_eng)
+print("General Business :",General_bus)
+print("Introduction to Computer Systems :",Intro_com)
+print("Computer Programming :",Com_prog)
+
